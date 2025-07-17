507411212d5957d717ac0250850da95d1343da32
mkk63 <mkk63@cornell.edu>
2025-07-17 13:31:52 -0700

    basic dashboard

--- dashboard.py
+++ dashboard.py
@@ -9,7 +9,7 @@ st.title("Dashboard")
 st.sidebar.text("sidebar")
 
 #selected_product = st.selectbox("Select Product:", df['product_name'].unique())
-selected_product = st.text_input("Enter Product Name:", value=df['product_name'].unique()[0])
+selected_product = st.text_input("Enter Product Name:", value="Product 1")
 if selected_product not in df['product_name'].unique():
     st.error("Invalid product name. Please enter a valid product name.")
     st.stop()
@@ -19,6 +19,7 @@ fig = go.Figure()
 fig.add_trace(go.Scatter(x=product_data['date'], y=product_data['demand'], name='Demand', line=dict(color='red')))
 fig.add_trace(go.Scatter(x=product_data['date'], y=product_data['forecast'], name='Forecast', line=dict(color='blue')))
 fig.update_layout(title=f'{selected_product}', xaxis_title='Date', yaxis_title='Quantity', height=400)
+
 
 st.plotly_chart(fig, use_container_width=True)
 
